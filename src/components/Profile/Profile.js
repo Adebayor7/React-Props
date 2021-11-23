3992188e7b8a9aac83606a7e0f7ba1212659ab78
Adebayor <idrisadeebayorr12@gmail.com>
2021-11-23 18:10:30 +0100

    some changes made

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,11 +8,10 @@ function Profile(props) {
     alert(fullName);
   };
   return (
-      
     <div>
       {props.children}
       <h2>Full Name: {fullName}</h2>
-      <h2>Bio: {bio}</h2>
+      <h3>Bio: {bio}</h3>
       <h2>Profession: {profession}</h2>
       <button onClick={handleName}>Click</button>
     </div>
@@ -20,8 +19,8 @@ function Profile(props) {
 }
 
 Profile.defaultProps = {
-  fullName: "Adebayo",
-  bio: "I'm a boy",
+  fullName: "Adebayo Idris",
+  bio: "I am a young man trying to achieve my goals and desire as a Full-stack software developer. There is way lot to say about myself but if am to keep it short i can say i am pretty honest and a blunt person because i do not hide my feelings from anything or anyone. Life is simple when you live it simple, we live once so i try to get the best out of life before we go 6feet under..",
   profession: "Software developer",
 };
 
